311ad2beeebe7c1d382aecb3986a188bb3b4ea77
Sarthak Agarwal <sarthakroxxx@gmail.com>
2025-06-18 03:09:37 +0530

    setup project with auth

---
+++ lib/auth.ts
@@ -0,0 +1,20 @@
+import prisma from "@/lib/db";
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { nextCookies } from "better-auth/next-js";
+import { username } from "better-auth/plugins";
+
+export const auth = betterAuth({
+  database: prismaAdapter(prisma, {
+    provider: "postgresql",
+  }),
+  emailAndPassword: {
+    enabled: true,
+  },
+  advanced: {
+    database: {
+      generateId: false,
+    },
+  },
+  plugins: [username(), nextCookies()],
+});
